418af8a91d889fa5672802dfabe59e1094d721c5
dnkntksh <mr.kaz71@gmail.com>
2021-05-16 15:56:56 +0900

    springboot api 動作確認まで

--- backend/src/main/java/app/AppApplication.java
+++ backend/src/main/java/app/AppApplication.java
@@ -1,4 +1,4 @@
-package app.java.app;
+package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ backend/src/main/java/app/controller/HomeController.java
@@ -0,0 +1,13 @@
+package app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+    @RequestMapping("/")
+    public String user() {
+      return "home dir";
+    }
+}
